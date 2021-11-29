cac425f8fd3e5f0bf11fb6033fd657594e1663a1
TanyaUrlapkina <write2me92@mail.ru>
2021-11-29 22:50:33 +0300

    COVEREDRATIO 1.00

--- src/test/java/ru/netology/StatisticServiceTest.java
+++ src/test/java/ru/netology/StatisticServiceTest.java
@@ -10,8 +10,8 @@ class StatisticServiceTest {
     void findMax() {
         StatisticService service = new StatisticService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 32, 12};
+        long expected = 32;
 
         long actual = service.findMax(incomesInBillions);
 
